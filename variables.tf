fe52733dea9157c6d98f48b18bd9723b3a3bb015
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-23 10:06:44 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {}
+variable "gcp_project" {}
+variable "credentials" {}
+variable "rabbitmqvpc" {}
+variable "rabbitmqsub" {}
+// variable "elk_instance_name" {}
+variable "instance_name" {}
+variable "rabbitmq_instance_ip_name" {}
+variable "rabbitmq_instance_ip_region" {}
